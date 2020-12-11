8efa0d4648c3df482abb5b9b0ccab7a58b2271ac
Pavel Alexander Rivadeneira Cajas <pavel.rivadeneira@unl.edu.ec>
2020-12-11 14:34:20 -0500

    a

---
+++ Autenticacion/Data/AutenticacionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Autenticacion.Usuarios;
+
+namespace Autenticacion.Data
+{
+    public class AutenticacionContext : DbContext
+    {
+        public AutenticacionContext (DbContextOptions<AutenticacionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Autenticacion.Usuarios.Persona> Persona { get; set; }
+    }
+}
---
+++ Autenticacion/Migrations/20201211014313_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Autenticacion.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persona",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persona", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persona");
+        }
+    }
+}
